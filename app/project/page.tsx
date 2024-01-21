6460bb990db96437eafb322138d7fe8b28652f70
Mubeen-Anxari <mubeenansari10001@gmail.com>
2024-01-21 12:24:58 +0500

    project

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -7,8 +7,8 @@ export default function Project() {
       <h1 className="text-white font-bold text-2xl text-center">My Project </h1>
       <div className="flex gap-2 justify-center mt-6">
        <Link className=" bg-white px-6 hover:bg-lime-600 hover:text-black rounded-full text-black" href="/all">All</Link>
-       <Link className=" bg-white px-6 hover:bg-lime-600 hover:text-black rounded-full text-black" href="/2">Project</Link>
-       <Link className=" bg-white px-6 hover:bg-lime-600 hover:text-black rounded-full text-black" href="/4 ">Contact</Link>
+       <Link className=" bg-white px-6 hover:bg-lime-600 hover:text-black rounded-full text-black" href="/blog/2">Project</Link>
+       <Link className=" bg-white px-6 hover:bg-lime-600 hover:text-black rounded-full text-black" href="/blog/4 ">Contact</Link>
       </div>
      
 
